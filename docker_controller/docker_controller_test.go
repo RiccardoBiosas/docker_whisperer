6c5d6319a47f395612ca96aca7ae4a27dcb1b346
RiccardoBiosas <rbios@protonmail.com>
2022-09-29 14:46:14 +0200

    test: add unit test to validate building docker image from local dockerfile

---
+++ docker_controller/docker_controller_test.go
@@ -0,0 +1,37 @@
+package docker_controller
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestBuildImageWithRelativePath(t *testing.T) {
+	controller, err := NewController()
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = controller.BuildLocalImage("./dockerfile", "default")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBuildImageWithAbsolutePath(t *testing.T) {
+	controller, err := NewController()
+	if err != nil {
+		t.Error(err)
+	}
+
+	path, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+	pathElements := []string{path, "dockerfile"}
+	concatenatedPath := strings.Join(pathElements, "/")
+	fmt.Println(concatenatedPath)
+
+	err = controller.BuildLocalImage(concatenatedPath, "default")
+}
